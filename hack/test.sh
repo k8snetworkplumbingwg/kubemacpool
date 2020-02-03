7dca7d48edf23509ee15b3a5e7a54ef0490b87ba
Petr Horacek <phoracek@redhat.com>
2020-02-03 18:42:39 +0100

    drop dockerized test
    We can run the tests with Go alone, there is no need to containerize them.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- hack/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --rm -t --volume `pwd`:/go/src/github.com/k8s-nativelb  --volume $HOME/.kube/:/root/.kube/ --volume $HOME/.minikube:/home/travis/.minikube --env KUBECONFIG=/root/.kube/config --workdir /go/src/github.com/k8s-nativelb/ golang:1.11.0 make test

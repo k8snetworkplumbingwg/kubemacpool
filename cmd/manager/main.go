589c603669a4f8a36c453f1b67d7230d7b6b0431
Or Mergi <ormergi@redhat.com>
2020-04-27 15:54:37 +0300

    Simplicy Main function
    Extract to function log level handaling and loading enviroment variables
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -17,10 +17,12 @@ limitations under the License.
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 
@@ -28,65 +30,80 @@ import (
 	poolmanager "github.com/k8snetworkplumbingwg/kubemacpool/pkg/pool-manager"
 )
 
-func loadMacAddressFromEnvVar(envName string) (net.HardwareAddr, error) {
-	if value, ok := os.LookupEnv(envName); ok {
-		poolRange, err := net.ParseMAC(value)
-		if err != nil {
-			return nil, err
-		}
-
-		return poolRange, nil
-	}
-
-	return nil, fmt.Errorf("Environment variable %s don't exist", envName)
-}
+var log = logf.Log.WithName("Main")
 
 func main() {
 	var logType, metricsAddr string
 	var waitingTime int
+	envVariables := map[string]string{
+		"rangeStart":     poolmanager.RangeStartEnv,
+		"rangeEnd":       poolmanager.RangeEndEnv,
+		"shardingFactor": poolmanager.ShardingFactor,
+		"podNamespace":   "POD_NAMESPACE",
+		"podName":        "POD_NAME",
+	}
 
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&logType, "v", "production", "Log type (debug/production).")
 	flag.IntVar(&waitingTime, "wait-time", 600, "waiting time to release the mac if object was not created")
 	flag.Parse()
 
+	setLogLevel(logType)
+
+	err := loadEnvironmentVariables(envVariables)
+	if err != nil {
+		log.Error(err, "Failed to load environment variables")
+		os.Exit(1)
+	}
+
+	macRangeHwAddresses, err := parseRangeAddresses([]string{"rangeStart", "rangeEnd"}, envVariables)
+	if err != nil {
+		log.Error(err, "Failed to parse mac range address")
+		os.Exit(1)
+	}
+
+	kubemacpoolManager := manager.NewKubeMacPoolManager(envVariables["podNamespace"], envVariables["podName"], metricsAddr, waitingTime)
+	shardingFactor, err := strconv.Atoi(envVariables["shardingFactor"])
+	if err != nil {
+		log.Error(err, "Failed to parse sharding factor")
+		os.Exit(1)
+	}
+
+	err = kubemacpoolManager.Run(macRangeHwAddresses["rangeStart"], macRangeHwAddresses["rangeEnd"], shardingFactor)
+	if err != nil {
+		log.Error(err, "Failed to run the manager")
+		os.Exit(1)
+	}
+}
+
+func setLogLevel(logType string) {
 	if logType == "debug" {
 		logf.SetLogger(logf.ZapLogger(true))
 	} else {
 		logf.SetLogger(logf.ZapLogger(false))
 	}
+}
 
-	log := logf.Log.WithName("main")
+func loadEnvironmentVariables(envVariables map[string]string) error {
+	for envVarName, _ := range envVariables {
+		envVarValue, ok := os.LookupEnv(envVariables[envVarName])
+		if !ok {
+			return errors.New(fmt.Sprintf("Faild to load enviroment variable %s: %s", envVarName, envVariables[envVarName]))
+		}
+		envVariables[envVarName] = envVarValue
+	}
+	return nil
+}
 
-	rangeStart, err := loadMacAddressFromEnvVar(poolmanager.RangeStartEnv)
-	if err != nil {
-		log.Error(err, "Failed to load mac address from environment variable")
-		os.Exit(1)
+func parseRangeAddresses(variableNames []string, variablesMap map[string]string) (map[string]net.HardwareAddr, error) {
+	macAddressMap := map[string]net.HardwareAddr{}
+	for _, varName := range variableNames {
+		macAddress, err := net.ParseMAC(variablesMap[varName])
+		if err != nil {
+			return nil, fmt.Errorf("Failed to parse %s, %s", varName, err)
+		}
+		macAddressMap[varName] = macAddress
 	}
 
-	rangeEnd, err := loadMacAddressFromEnvVar(poolmanager.RangeEndEnv)
-	if err != nil {
-		log.Error(err, "Failed to load mac address from environment variable")
-		os.Exit(1)
-	}
-
-	podNamespace, ok := os.LookupEnv("POD_NAMESPACE")
-	if !ok {
-		log.Error(err, "Failed to load pod namespace from environment variable")
-		os.Exit(1)
-	}
-
-	podName, ok := os.LookupEnv("POD_NAME")
-	if !ok {
-		log.Error(err, "Failed to load pod name from environment variable")
-		os.Exit(1)
-	}
-
-	kubemacpoolManager := manager.NewKubeMacPoolManager(podNamespace, podName, metricsAddr, waitingTime)
-
-	err = kubemacpoolManager.Run(rangeStart, rangeEnd)
-	if err != nil {
-		log.Error(err, "Failed to run the manager")
-		os.Exit(1)
-	}
+	return macAddressMap, nil
 }

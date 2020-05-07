7594c8ab066650b5fa87307d5efff27c14a4a27e
Quique Llorente <ellorent@redhat.com>
2020-05-07 10:24:46 +0200

    Vendor all the golang tools
    They are included at tools.go so `make vendor` will vendor them later
    is possible to call `make tools` to install them at GOBIN call them directly
    without the docker-builder container, this simplify the infra and make upadting
    tools as simple as updating another golang dep at go.mod, also kustomize has
    being updated.
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- hack/goveralls.sh
+++ hack/goveralls.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-goveralls -coverprofile=cover.out -service=travis-ci -repotoken $COVERALLS_TOKEN -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
+$GOVERALLS -coverprofile=cover.out -service=travis-ci -repotoken $COVERALLS_TOKEN -ignore=$(find -regextype posix-egrep -regex ".*generated_mock.*\.go|.*swagger_generated\.go|.*openapi_generated\.go" -printf "%P\n" | paste -d, -s)
--- hack/install-tools.sh
+++ hack/install-tools.sh
@@ -1,14 +1,6 @@
 #!/bin/bash -xe
-export GOFLAGS=-mod=vendor
-export GO111MODULE=on
-
 tools_file=$1
-
 tools=$(grep "_" $tools_file |  sed 's/.*_ *"//' | sed 's/"//g')
-go mod tidy
-go mod vendor
 for tool in $tools; do
-    go install $tool
+    $GO install ./vendor/$tool
 done
-go mod vendor
-

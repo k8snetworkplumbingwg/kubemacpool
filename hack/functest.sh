d982f263097d7280acc20fb618dcf409dd6047b6
Quique Llorente <ellorent@redhat.com>
2020-05-27 12:05:28 +0200

    Revert ginkgo cmd but keep ginkgo verbose
    If we use "ginkgo" command we have to pass extra args at two different places
    in the command:
    
    ginkgo $GINKGO_ARGS -- $TEST_ARGS
    
    so we need two flags at KMP build env, since we can use `go test` instead but keep ginkgo verbose
    with `-ginkgo.v` let's just use "go test" gain so we can do
    
    go test $TEST_ARGS
    
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- hack/functest.sh
+++ hack/functest.sh
@@ -4,4 +4,4 @@ set -xe
 
 source ./cluster/kubevirtci.sh
 
-KUBECONFIG=${KUBECONFIG:-$(kubevirtci::kubeconfig)} $GINKGO -timeout=20m -v -race ./tests/...
+KUBECONFIG=${KUBECONFIG:-$(kubevirtci::kubeconfig)} $GO test ./tests/... -timeout=20m -ginkgo.v -test.v -race

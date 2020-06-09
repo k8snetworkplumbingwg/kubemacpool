e28811becb6bb9614308ccee7a4b9fb08d0cfa1d
Ram Lavi <ralavi@redhat.com>
2020-06-09 19:08:31 +0300

    adding a new readinessGate status to kubemacpool pods.
    since the webhook service operates only on pods with kubemacpool leader label, we cant start the webhooks until the election is complete and a leader as been chosen.
    in order to do that, we add a new status to the pod, to prevent the pods to be ready and the webhhoks to start until the label is set.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/manager/leaderelection.go
+++ pkg/manager/leaderelection.go
@@ -2,15 +2,32 @@ package manager
 
 import (
 	"context"
-	"github.com/k8snetworkplumbingwg/kubemacpool/pkg/names"
+
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/retry"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/k8snetworkplumbingwg/kubemacpool/pkg/names"
 )
 
 func (k *KubeMacPoolManager) waitToStartLeading() error {
 	<-k.mgr.Elected()
 	// If we reach here then we are in the elected pod.
-	return k.markPodAsLeader()
+
+	err := k.markPodAsLeader()
+	if err != nil {
+		log.Error(err, "failed marking pod as leader")
+		return err
+	}
+
+	err = k.setLeadershipConditions(corev1.ConditionTrue)
+	if err != nil {
+		log.Error(err, "failed changing leadership condition to true")
+		return err
+	}
+	return nil
 }
 
 func (k *KubeMacPoolManager) markPodAsLeader() error {
@@ -31,5 +48,35 @@ func (k *KubeMacPoolManager) markPodAsLeader() error {
 	}
 
 	log.Info("marked this manager as leader for webhook", "podName", k.podName)
+	return nil
+}
+
+// By setting this status to true in all pods, we declare the kubemacpool as ready and allow the webhooks to start running.
+func (k *KubeMacPoolManager) setLeadershipConditions(status corev1.ConditionStatus) error {
+	podList := corev1.PodList{}
+	err := k.mgr.GetClient().List(context.TODO(), &podList, &client.ListOptions{Namespace: k.podNamespace})
+	if err != nil {
+		return err
+	}
+	for _, pod := range podList.Items {
+		err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			podKey := types.NamespacedName{Namespace: k.podNamespace, Name: pod.Name}
+			err := k.mgr.GetClient().Get(context.TODO(), podKey, &pod)
+			if err != nil {
+				return err
+			}
+
+			pod.Status.Conditions = append(pod.Status.Conditions, corev1.PodCondition{Type: "kubemacpool.io/leader-ready", Status: status, LastProbeTime: metav1.Time{}})
+
+			err = k.mgr.GetClient().Status().Update(context.TODO(), &pod)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }

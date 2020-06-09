c758324cea695ef01217f3f39fc5adca8180a2c2
Ram Lavi <ralavi@redhat.com>
2020-06-09 17:29:58 +0300

    add mgr to KubeMacPoolManager and use mgr.Elected() to mark only the elected leader pod This also means we get can remove the use of leaderElectionChannel
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/manager/leaderelection.go
+++ pkg/manager/leaderelection.go
@@ -2,71 +2,13 @@ package manager
 
 import (
 	"context"
-	"time"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/leaderelection"
-	manager_leaderelection "sigs.k8s.io/controller-runtime/pkg/leaderelection"
-
 	"github.com/k8snetworkplumbingwg/kubemacpool/pkg/names"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func (k *KubeMacPoolManager) newLeaderElection(config *rest.Config, scheme *runtime.Scheme) error {
-	recorderProvider, err := NewProvider(config, scheme, log.WithName("events"))
-	if err != nil {
-		return err
-	}
-
-	// Create the resource lock to enable leader election)
-	resourceLock, err := manager_leaderelection.NewResourceLock(config, recorderProvider, manager_leaderelection.Options{
-		LeaderElection:          true,
-		LeaderElectionID:        names.LEADER_ID,
-		LeaderElectionNamespace: k.podNamespace,
-	})
-	if err != nil {
-		return err
-	}
-
-	l, err := leaderelection.NewLeaderElector(leaderelection.LeaderElectionConfig{
-		Lock: resourceLock,
-		// Values taken from: https://github.com/kubernetes/apiserver/blob/master/pkg/apis/config/v1alpha1/defaults.go
-		LeaseDuration: 15 * time.Second,
-		RenewDeadline: 10 * time.Second,
-		RetryPeriod:   2 * time.Second,
-		Callbacks: leaderelection.LeaderCallbacks{
-			OnStartedLeading: func(_ context.Context) {
-				k.leaderElectionChannel <- nil
-			},
-			OnStoppedLeading: func() {
-				k.restartChannel <- struct{}{}
-			},
-		},
-	})
-	if err != nil {
-		return err
-	}
-
-	k.leaderElection = l
-
-	// Start the leader elector process
-	go k.leaderElection.Run(context.Background())
-
-	return nil
-}
-
 func (k *KubeMacPoolManager) waitToStartLeading() error {
-	if k.leaderElection.IsLeader() {
-		log.Info("This manager is already the leader")
-		k.leaderElectionChannel <- nil
-	}
-
-	err := <-k.leaderElectionChannel
-	if err != nil {
-		return err
-	}
-
+	<-k.mgr.Elected()
+	// If we reach here then we are in the elected pod.
 	return k.markPodAsLeader()
 }
 

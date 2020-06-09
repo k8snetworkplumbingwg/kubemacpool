c5d341fb824d93ffc1d2d2590f8138f4e411bf88
Ram Lavi <ralavi@redhat.com>
2020-06-09 17:41:47 +0300

    add retryOnConflict to better handle future conflict in markPodAsLeader()
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/manager/leaderelection.go
+++ pkg/manager/leaderelection.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/k8snetworkplumbingwg/kubemacpool/pkg/names"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/util/retry"
 )
 
 func (k *KubeMacPoolManager) waitToStartLeading() error {
@@ -13,13 +14,18 @@ func (k *KubeMacPoolManager) waitToStartLeading() error {
 }
 
 func (k *KubeMacPoolManager) markPodAsLeader() error {
-	pod, err := k.clientset.CoreV1().Pods(k.podNamespace).Get(context.TODO(), k.podName, metav1.GetOptions{})
-	if err != nil {
-		return err
-	}
+	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		pod, err := k.clientset.CoreV1().Pods(k.podNamespace).Get(context.TODO(), k.podName, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
 
-	pod.Labels[names.LEADER_LABEL] = "true"
-	_, err = k.clientset.CoreV1().Pods(k.podNamespace).Update(context.TODO(), pod, metav1.UpdateOptions{})
+		pod.Labels[names.LEADER_LABEL] = "true"
+
+		_, err = k.clientset.CoreV1().Pods(k.podNamespace).Update(context.TODO(), pod, metav1.UpdateOptions{})
+		return err
+	})
+
 	if err != nil {
 		return err
 	}

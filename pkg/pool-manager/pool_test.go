e07a9844227f1ad736bf76c7fb1ad243eea9f8d2
Ram Lavi <ralavi@redhat.com>
2020-06-16 10:31:51 +0300

    remove rebug print merged by mistake in pool_test.go
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/pool-manager/pool_test.go
+++ pkg/pool-manager/pool_test.go
@@ -403,7 +403,6 @@ var _ = Describe("Pool", func() {
 				By("checking the configmap is updated with mac allocated")
 				macAddressInConfigMapFormat := strings.Replace(allocatedMac, ":", "-", 5)
 				Expect(configMap.Data).To(HaveLen(1),"configmap should hold the mac address waiting for approval")
-				fmt.Printf("configMap Data %v\n", configMap.Data)
 				_, exist := configMap.Data[macAddressInConfigMapFormat]
 				Expect(exist).To(Equal(true), "should have an entry of the mac in the configmap")
 			})

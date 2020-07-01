c797153a13c759780c56bc76b77847987b292d49
Quique Llorente <ellorent@redhat.com>
2020-07-01 09:31:55 +0200

    Remove Ginkgo CLI
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- tools.go
+++ tools.go
@@ -4,7 +4,6 @@ package tools
 
 import (
 	_ "github.com/mattn/goveralls"
-	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "sigs.k8s.io/kustomize/kustomize/v3"

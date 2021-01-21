4f7114275ceb28d243c226b1cd07a563039ce035
Ram Lavi <ralavi@redhat.com>
2021-01-21 11:34:37 +0200

    config, update kubemacpool manifests kubevirt.io v1
    In order to succsesfuly register kubevirt v1 [1], we also need to add
    an env var KUBEVIRT_CLIENT_GO_SCHEME_REGISTRATION_VERSION that defines
    the desired version to be registered.
    This env var needs to be set before the container starts.
    Also added the env var to test environment.
    
    [1] kubevirt/kubevirt#4842
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- hack/functest.sh
+++ hack/functest.sh
@@ -4,4 +4,5 @@ set -xe
 
 source ./cluster/kubevirtci.sh
 
+export KUBEVIRT_CLIENT_GO_SCHEME_REGISTRATION_VERSION=v1
 KUBECONFIG=${KUBECONFIG:-$(kubevirtci::kubeconfig)} $GO test -test.timeout=40m -race -test.v ./tests/... $E2E_TEST_ARGS -ginkgo.v --test-suite-params="$POLARION_TEST_SUITE_PARAMS"

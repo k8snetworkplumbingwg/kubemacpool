2ab8779ed24be8d5688dd1e8c8ec32ba81769221
Ram Lavi <ralavi@redhat.com>
2021-08-26 07:34:57 +0300

    Update KUBEVIRT_PROVIDER to k8s-1.21
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.19'}
+export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.21'}
 export KUBEVIRTCI_TAG='2108251059-2c2862b'
 
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'

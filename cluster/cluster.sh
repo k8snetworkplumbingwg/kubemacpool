48e7ca530d9c3ceccc926e97d2b7ad57ce7c2526
Ram Lavi <ralavi@redhat.com>
2024-09-08 21:07:34 +0300

    cluster: Bump kubevirtci tag
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 export KUBEVIRT_PROVIDER=${KUBEVIRT_PROVIDER:-'k8s-1.25'}
-export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-2211021552-8cca8c0}
+export KUBEVIRTCI_TAG=${KUBEVIRTCI_TAG:-2408151028-a0ad3359}
 
 KUBEVIRTCI_REPO='https://github.com/kubevirt/kubevirtci.git'
 # The CLUSTER_PATH var is used in cluster folder and points to the _kubevirtci where the cluster is deployed from.

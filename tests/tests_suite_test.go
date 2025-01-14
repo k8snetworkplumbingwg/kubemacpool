8d164e487899ed3ff4675ec614d6d140a3cfe6ba
Ram Lavi <ralavi@redhat.com>
2025-01-14 13:05:00 +0200

    e2e, failed-tests: Fix Logs
    Currently during test fail, the kubemacpool manager pod logs are
    not printed because the container name is not specified.
    - Changing method of extracting the logs using req.Stream.
    - Changing to print all container logs.
    - Fix minor print formatting.
    - move to functions for easier reading.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- tests/tests_suite_test.go
+++ tests/tests_suite_test.go
@@ -1,8 +1,10 @@
 package tests
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -35,6 +37,31 @@ var _ = AfterSuite(func() {
 	removeTestNamespaces()
 })
 
+func getPodContainerLogs(podName, containerName string) (string, error) {
+	req := testClient.VirtClient.CoreV1().Pods(managerNamespace).GetLogs(podName, &corev1.PodLogOptions{
+		Container: containerName,
+	})
+	podLogs, err := req.Stream(context.TODO())
+	if err != nil {
+		return "", err
+	}
+	defer func(podLogs io.ReadCloser) {
+		err := podLogs.Close()
+		if err != nil {
+			return
+		}
+	}(podLogs)
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, podLogs)
+	if err != nil {
+		return "", err
+	}
+	output := buf.String()
+
+	return output, nil
+}
+
 func KubemacPoolFailedFunction(message string, callerSkip ...int) {
 	podList, err := testClient.VirtClient.CoreV1().Pods(managerNamespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
@@ -49,33 +76,57 @@ func KubemacPoolFailedFunction(message string, callerSkip ...int) {
 			Fail(message, callerSkip...)
 		}
 
-		req := testClient.VirtClient.CoreV1().Pods(managerNamespace).GetLogs(pod.Name, &corev1.PodLogOptions{})
-		output, err := req.DoRaw(context.TODO())
-		if err != nil {
+		fmt.Printf("\nPod Name: %s \n", pod.Name)
+		fmt.Printf("Pod Yaml:\n%v \n", *podYaml)
+
+		if err = printPodContainersLogs(pod.Name, pod.Spec.Containers); err != nil {
 			fmt.Println(err)
 			Fail(message, callerSkip...)
 		}
-
-		fmt.Printf("Pod Name: %s \n", pod.Name)
-		fmt.Printf("%v \n", *podYaml)
-		fmt.Println(string(output))
 	}
 
-	service, err := testClient.VirtClient.CoreV1().Services(managerNamespace).Get(context.TODO(), names.WEBHOOK_SERVICE, metav1.GetOptions{})
-	if err != nil {
+	if err = printService(managerNamespace, names.WEBHOOK_SERVICE); err != nil {
 		fmt.Println(err)
 		Fail(message, callerSkip...)
 	}
 
-	fmt.Printf("Service: %v", service)
-
-	endpoint, err := testClient.VirtClient.CoreV1().Endpoints(managerNamespace).Get(context.TODO(), names.WEBHOOK_SERVICE, metav1.GetOptions{})
-	if err != nil {
+	if err = printEndpoints(managerNamespace, names.WEBHOOK_SERVICE); err != nil {
 		fmt.Println(err)
 		Fail(message, callerSkip...)
 	}
-
-	fmt.Printf("Endpoint: %v", endpoint)
 
 	Fail(message, callerSkip...)
+}
+
+func printPodContainersLogs(podName string, containers []corev1.Container) error {
+	for i := range containers {
+		containerName := containers[i].Name
+		podLogs, err := getPodContainerLogs(podName, containerName)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("\nPod container %q Logs:\n%s \n", containerName, podLogs)
+	}
+	return nil
+}
+
+func printService(serviceNamespace, serviceName string) error {
+	service, err := testClient.VirtClient.CoreV1().Services(serviceNamespace).Get(context.TODO(), serviceName, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Service: %v\n", service)
+	return nil
+}
+
+func printEndpoints(endpointNamespace, endpointName string) error {
+	endpoint, err := testClient.VirtClient.CoreV1().Endpoints(endpointNamespace).Get(context.TODO(), endpointName, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Endpoint: %v\n", endpoint)
+	return nil
 }

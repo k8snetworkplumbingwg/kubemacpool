d7a121725ae623be1077002bba5b6c643452e231
RamLavi <ralavi@redhat.com>
2025-05-08 08:49:54 +0300

    tests: log to artifacts on test failures (#535)
    currently e2e test prints the logs to the screen.
    Moving the logs to be written to artifacts.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

---
+++ tests/reporter/writers.go
@@ -0,0 +1,36 @@
+/*
+Copyright 2025 The KubeMacPool Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package reporter
+
+import (
+	"fmt"
+	"os"
+)
+
+func LogToFile(topic, logBody, artifactDir string, failureCount int) error {
+	fileName := fmt.Sprintf(artifactDir+"%d_%s.log", failureCount, topic)
+	file, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("Error creating log file %v, err %w\n", fileName, err)
+	}
+	defer file.Close()
+
+	if _, err = fmt.Fprint(file, logBody); err != nil {
+		fmt.Printf("Error writing log %s to file, err %v\n", fileName, err)
+	}
+	return nil
+}

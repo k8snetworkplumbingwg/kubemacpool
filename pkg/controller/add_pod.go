bb19bc23eb5e953ed05a0892da56b9c1e402889c
RamLavi <ralavi@redhat.com>
2026-01-26 14:15:45 +0200

    Revert "Skip re-registration of already registered controllers (#590)" (#594)
    This reverts commit 332789e5842e7a118a3a840f7f9c4a4d3a5fe835.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- pkg/controller/add_pod.go
+++ pkg/controller/add_pod.go
@@ -21,8 +21,6 @@ import (
 )
 
 func init() {
-	AddToManagerFuncs = append(AddToManagerFuncs, ControllerAdder{
-		Name: "pod-controller",
-		Add:  pod.Add,
-	})
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, pod.Add)
 }
--- pkg/controller/virtualmachine/virtualmachine_controller.go
+++ pkg/controller/virtualmachine/virtualmachine_controller.go
@@ -43,14 +43,12 @@ var log = logf.Log.WithName("VirtualMachine Controller")
 
 // Add creates a new Policy Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
-func Add(mgr manager.Manager, poolManager *pool_manager.PoolManager) (bool, error) {
-	if !poolManager.IsKubevirtEnabled() {
-		return false, nil
+func Add(mgr manager.Manager, poolManager *pool_manager.PoolManager) error {
+	if poolManager.IsKubevirtEnabled() {
+		return add(mgr, newReconciler(mgr, poolManager))
 	}
-	if err := add(mgr, newReconciler(mgr, poolManager)); err != nil {
-		return false, err
-	}
-	return true, nil
+
+	return nil
 }
 
 // newReconciler returns a new reconcile.Reconciler
